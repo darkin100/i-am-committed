041ab088b4698607607b37950d6df7aae35e5b64
Glyn Darkin <me@glyndarkin.co.uk>
2025-02-24 21:22:09 +0000

    feat(formatter): enhance message formatting to remove backticks
    - Improved the format method in CommitFormatter to remove backticks from commit messages.
    - Added unit tests to ensure that messages with single and multi-line backticks are correctly formatted.

--- src/commit_formatter/formatter.rs
+++ src/commit_formatter/formatter.rs
@@ -10,7 +10,13 @@ impl CommitFormatter {
     }
 
     pub fn format(&self) -> CommitType {
-        CommitType::new(self.raw_message.trim().to_string())
+        // Remove backticks and trim whitespace
+        let cleaned_message = self.raw_message
+            .replace("`", "")
+            .trim()
+            .to_string();
+        
+        CommitType::new(cleaned_message)
     }
 }
 
@@ -23,5 +29,31 @@ mod tests {
         let message = "feat(auth): Add login functionality";
         let formatter = CommitFormatter::new(message.to_string());
         assert_eq!(formatter.format().to_string(), message);
+    }
+
+    #[test]
+    fn test_format_message_with_backticks() {
+        let message = "```feat(auth): Add login functionality```";
+        let formatter = CommitFormatter::new(message.to_string());
+        assert_eq!(formatter.format().to_string(), "feat(auth): Add login functionality");
+    }
+
+    #[test]
+    fn test_format_message_with_single_backticks() {
+        let message = "`feat(core)`: Add new feature";
+        let formatter = CommitFormatter::new(message.to_string());
+        assert_eq!(formatter.format().to_string(), "feat(core): Add new feature");
+    }
+
+    #[test]
+    fn test_format_message_with_multiline_backticks() {
+        let message = "feat(core): Add new feature
+        ```
+        added new feature
+        ```";
+        let formatter = CommitFormatter::new(message.to_string());
+        assert_eq!(formatter.format().to_string(), "feat(core): Add new feature
+        
+        added new feature");
     }
 }
